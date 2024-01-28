348dec9610608e5df97d337ecedbcd2f8643650d
Vinícius Matheus <125493286+DvlprMatheus@users.noreply.github.com>
2024-01-28 16:21:34 -0300

    Code Update
    Incrementação de novas funções, e algumas ligações com o back-end.

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Moment } from '../../../Moments';
+
+import { MomentService } from '../../../services/moment.service';
+import { MessagesService } from '../../../services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -7,4 +13,27 @@ import { Component } from '@angular/core';
 })
 export class NewMomentComponent {
   btnText = 'Compartilhar!'
+
+  constructor(
+    private momentService: MomentService, 
+    private messagesService: MessagesService, 
+    private router: Router
+    ) {}
+
+  async createHandler(moment : Moment) {
+    const formData = new FormData()
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add('Momento adicionado com sucesso!')
+
+    this.router.navigate(['/']);
+  }
 }
